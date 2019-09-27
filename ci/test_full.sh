8d5da48a7b882cd9533cef8fdcfceb3513396898
roblabla <unfiltered@roblab.la>
2019-09-27 11:52:15 +0000

    Only test nostd builds on nightly on travis

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -20,19 +20,23 @@ cargo test --no-default-features --features="std"
 rustup target add thumbv7m-none-eabi
 cargo build --no-default-features --target=thumbv7m-none-eabi
 
-# It should work in no_std
+# It should work in no_std on nightly.
 # Note: Doctest might show an error: https://github.com/rust-lang/rust/issues/54010
 # The "error" is wrong however, the doctests still run.
-cargo test --no-default-features
+if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  cargo test --no-default-features
+fi
 
 # Each isolated feature should also work everywhere.
 for feature in $FEATURES; do
   cargo build --verbose --no-default-features --features="std $feature"
   cargo test --verbose --no-default-features --features="std $feature"
 
-  # Ensure that feature also works in nostd context
-  cargo build --verbose --no-default-features --features="$feature"
-  cargo test --verbose --no-default-features --features="$feature"
+  # Ensure that feature also works in nostd context on nightly.
+  if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+    cargo build --verbose --no-default-features --features="$feature"
+    cargo test --verbose --no-default-features --features="$feature"
+  fi
 done
 
 # test all supported features together
